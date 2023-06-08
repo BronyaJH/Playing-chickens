d506e020f3be5c0e6a3f98b18e32dc53d90ac785
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-06-08 12:20:44 +0800

    enemy animation

---
+++ dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Animator animator;
+    public ParticleSystem attackPs;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TestAttack();
+        }
+
+    }
+
+    void TestAttack()
+    {
+        animator.SetTrigger("attack");
+    }
+
+    public void PerformAttack()
+    {
+        Debug.Log("attack");
+        attackPs.Play();
+    }
+}
