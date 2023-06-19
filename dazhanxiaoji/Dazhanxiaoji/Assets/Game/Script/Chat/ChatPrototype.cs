43a16dfdec9b2697fd45ae3511107fd8c1b69145
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-06-19 17:24:00 +0800

    add common scripts

---
+++ dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/Chat/ChatPrototype.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ChatPrototype : ScriptableObject
+{
+    public string content;
+    public string soundName;
+
+    public string characterId;
+    public ChatSpecialAction chatSpecialAction;
+
+    public ChatPrototype next;
+    public enum ChatSpecialAction
+    {
+        None,
+
+    }
+}
