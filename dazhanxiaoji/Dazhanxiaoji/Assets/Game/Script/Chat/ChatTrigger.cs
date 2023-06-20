3890a3055141506a1760c54965262efbfb21b255
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-06-20 11:29:50 +0800

    camera setting up and chat preparations

---
+++ dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/Chat/ChatTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ChatTrigger : MonoBehaviour
+{
+    public ChatPrototype chat;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ChatSystem.instance.ShowChat(chat);
+        }
+    }
+}
