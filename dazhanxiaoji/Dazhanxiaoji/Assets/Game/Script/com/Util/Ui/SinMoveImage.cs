2d3ff0dd1ee3e5ba983dbb55257382dfead7e8c6
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-06-20 11:46:48 +0800

    chat demo

--- dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/com/Util/Ui/SinMoveImage.cs
+++ dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/com/Util/Ui/SinMoveImage.cs
@@ -8,22 +8,24 @@ namespace com
         public RectTransform trans;
         public float amplitude;
         public Vector2 baseSpeed;
+
+        public bool useUnscaledTime = true;
         public bool useRawTime = true;
 
         private Vector2 _anchoredPos;
+
         private void Start()
         {
             if (trans == null)
-            {
                 trans = GetComponent<RectTransform>();
-            }
 
             _anchoredPos = trans.anchoredPosition;
         }
 
         private void Update()
         {
-            trans.anchoredPosition = _anchoredPos + baseSpeed * amplitude * Mathf.Sin((useRawTime ? Time.time : com.GameTime.time) * freq);
+            var t = useUnscaledTime ? Time.unscaledTime : (useRawTime ? Time.time : com.GameTime.time);
+            trans.anchoredPosition = _anchoredPos + baseSpeed * amplitude * Mathf.Sin(t * freq);
         }
     }
 }
