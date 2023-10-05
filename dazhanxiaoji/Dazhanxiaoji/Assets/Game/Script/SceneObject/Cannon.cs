b77b97287123f897b8ffc9a05a4d36577bfce88e
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-10-05 09:49:04 +0800

    Update Cannon.cs

--- dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/SceneObject/Cannon.cs
+++ dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/SceneObject/Cannon.cs
@@ -1,52 +1,51 @@
 ﻿using com;
-using System.Collections;
 using UnityEngine;
 
-namespace Assets.Game.Script.SceneObject
+public class Cannon : MonoBehaviour
 {
+    public GameObject bulletPrefab;
 
-    public class Cannon : MonoBehaviour
+    public float interval;
+
+    public Transform muzzle;
+
+    public bool activated;//minion died or not
+    public float distanceXFromPlayer = 30;
+
+    private float _nextOpenFireTime;
+    public EnemyBehaviour minion;
+
+    private void Start()
     {
-        public GameObject bulletPrefab;
+        _nextOpenFireTime = Time.time + interval;
+    }
 
-        public float interval;
+    private void Update()
+    {
+        if (!activated)
+            return;
 
-        public Transform muzzle;
+        if (minion == null || minion.IsDead)
+            return;
 
-        public bool activated;//minion died or not
-        public float distanceXFromPlayer = 30;
+        var player = PlayerBehaviour.instance;
+        if (player == null)
+            return;
+        var dir = player.transform.position - transform.position;
+        dir.y = 0;
+        dir.z = 0;
+        var dist = dir.magnitude;
+        if (dist > distanceXFromPlayer)
+            return;
 
-        private float _nextOpenFireTime;
+        if (Time.time > _nextOpenFireTime)
+            Fire();
+    }
 
-        private void Start()
-        {
-            _nextOpenFireTime = Time.time + interval;
-        }
-
-        private void Update()
-        {
-            if (!activated)
-                return;
-
-            var player = PlayerBehaviour.instance;
-            if (player == null)
-                return;
-            var dir = player.transform.position - transform.position;
-            dir.y = 0;
-            dir.z = 0;
-            var dist = dir.magnitude;
-            if (dist > distanceXFromPlayer)
-                return;
-
-            if (Time.time > _nextOpenFireTime)
-                Fire();
-        }
-
-        void Fire()
-        {
-            SoundSystem.instance.Play("FireCannon");
-            Instantiate(bulletPrefab, muzzle.transform.position, muzzle.transform.rotation);
-            _nextOpenFireTime = Time.time + interval;
-        }
+    void Fire()
+    {
+        SoundSystem.instance.Play("FireCannon");
+        Instantiate(bulletPrefab, muzzle.transform.position, muzzle.transform.rotation);
+        _nextOpenFireTime = Time.time + interval;
     }
 }
