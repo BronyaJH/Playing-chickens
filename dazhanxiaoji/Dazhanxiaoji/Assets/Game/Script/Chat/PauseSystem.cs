18369208af75608714dc814c5ae59e837027510d
Aieylisyh <Aieylisyh@users.noreply.github.com>
2023-11-02 13:16:58 +0800

    wip

--- dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/Chat/PauseSystem.cs
+++ dazhanxiaoji/Dazhanxiaoji/Assets/Game/Script/Chat/PauseSystem.cs
@@ -5,6 +5,8 @@ public class PauseSystem : MonoBehaviour
 {
     public static PauseSystem instance { get; private set; }
 
+    public int w;
+    public int h;
     private void Awake()
     {
         instance = this;
@@ -18,5 +20,13 @@ public class PauseSystem : MonoBehaviour
     public void Resume()
     {
         Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ScreenshotHandler.TakeScreenshot_Static(w, h);
+        }
     }
 }
